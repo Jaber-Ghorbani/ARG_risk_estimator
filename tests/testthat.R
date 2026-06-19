5066d83b4940009ef8e371d2caf2394bd609466c
Jaber Ghorbani <122388150+Jaber-Ghorbani@users.noreply.github.com>
2026-06-19 14:04:04 -0500

    Source helper scripts before tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,15 @@
 library(testthat)
+library(dplyr)
+library(readr)
+library(stringr)
+library(tibble)
+library(ggplot2)
+
+source("R/input_validation.R")
+source("R/score_lookup.R")
+source("R/index_calculation.R")
+source("R/plots.R")
+source("R/report_download.R")
 
 testthat::test_dir(
   path = file.path("tests", "testthat"),
